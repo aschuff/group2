9331877cde30d40d16eebbd13dd10dc6a15e1191
David Ryan Delli-Gatti <Gatti@Davids-MacBook-Pro.local>
2016-06-15 13:57:46 -0400

    es6'd horse constructor

--- horses.js
+++ horses.js
@@ -5,20 +5,37 @@
 var horseObject = {
   init: function() {
 
-    function Horse(opts) {
-      this.name = opts.name;
-      this.speed = opts.speed;
-      this.healthWeapon = opts.healthWeapon;
-      this.balanceWeapon = opts.balanceWeapon;
-      this.healthWeaponAffect = Math.floor(Math.random() * (14) + 20;
-      this.balanceWeaponAffect = Math.floor(Math.random() * (14) + 20;
+    class Horse {
+      constructor(opts) {
+        this.name = opts.name;
+        this.speed = opts.speed;
+        this.healthWeapon = opts.healthWeapon;
+        this.balanceWeapon = opts.balanceWeapon;
+        this.healthWeaponAffect = Math.floor(Math.random() * (14) + 20);
+        this.balanceWeaponAffect = Math.floor(Math.random() * (14) + 20);
+      }
+
+      affectBalance(char){
+        char.balance = char.balance - this.balanceWeaponAffect;
+      }
+      affectHealth(char){
+        char.health = char.health - this.healthWeaponAffect;
+      }
     }
-    Horse.prototype.affectBalance = function(char) {
-      char.balance = char.balance - this.balanceWeaponAffect;
-    }
-    Horse.prototype.affectHealth = function(char) {
-      char.health = char.health - this.healthWeaponAffect;
-    }
-  }
-  
-}
+    //
+    // function Horse(opts) {
+    //   this.name = opts.name;
+    //   this.speed = opts.speed;
+    //   this.healthWeapon = opts.healthWeapon;
+    //   this.balanceWeapon = opts.balanceWeapon;
+    //   this.healthWeaponAffect = Math.floor(Math.random() * (14) + 20);
+    //   this.balanceWeaponAffect = Math.floor(Math.random() * (14) + 20);
+    // }
+    // Horse.prototype.affectBalance = function(char) {
+    //   char.balance = char.balance - this.balanceWeaponAffect;
+    // }
+    // Horse.prototype.affectHealth = function(char) {
+    //   char.health = char.health - this.healthWeaponAffect;
+    // }
+//   }
+// }
